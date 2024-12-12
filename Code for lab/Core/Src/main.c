7cffcd66347a850be0716c4b55cfc40d3a8353d2
quangminh <lequangminh9768@gmail.com>
2024-12-13 02:31:37 +0700

    no message

--- Code for lab/Core/Src/main.c
+++ Code for lab/Core/Src/main.c
@@ -91,38 +91,89 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  int led_status = 1 ;
-  int count = 0;
-  while (1)
-   {
-	  switch(led_status){
-	  case 1:
-			HAL_GPIO_WritePin ( LED11_GPIO_Port, LED11_Pin , 0) ;
-			HAL_GPIO_WritePin (LED1_GPIO_Port, LED1_Pin, 1) ;
-			count++;
-			if(count >= 2){
-				led_status = 0;
-				count = 0;
-			}
+    int count = 1;
+    int count5s = 1;
 
-  	 break;
-	  case 0:
-			HAL_GPIO_WritePin ( LED11_GPIO_Port, LED11_Pin , 1) ;
-			HAL_GPIO_WritePin (LED1_GPIO_Port, LED1_Pin, 0) ;
-			count++;
-			if(count >=2 ){
-				led_status = 1;
-				count = 0;
-			}
+    while (1)
+    {
+  	  //led red trai va phai on
+  	  if(count <= 5){
+  		  HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, 1);//red
+  		  HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, 0);//green
+  		  HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, 0);//yellow
 
-		break;
-	  }
-  	  HAL_Delay (1000) ;
+  		  HAL_GPIO_WritePin(LED8_GPIO_Port, LED8_Pin, 1);
+  		  HAL_GPIO_WritePin(LED9_GPIO_Port, LED9_Pin, 0);
+  		  HAL_GPIO_WritePin(LED10_GPIO_Port, LED10_Pin, 0);
+  		  //led green tren va duoi on
+  		  if(count5s <= 3){
+  		  HAL_GPIO_WritePin(LED5_GPIO_Port, LED5_Pin, 0);
+  		  HAL_GPIO_WritePin(LED6_GPIO_Port, LED6_Pin, 1);
+  		  HAL_GPIO_WritePin(LED7_GPIO_Port, LED7_Pin, 0);
+  		  HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, 0);
+  		  HAL_GPIO_WritePin(LED12_GPIO_Port, LED12_Pin, 1);
+  		  HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, 0);
+  		  count5s++;
+  		  }
+  		  //led yellow tren va duoi on
+  		  else{
+  		  HAL_GPIO_WritePin(LED5_GPIO_Port, LED5_Pin, 0);
+  		  HAL_GPIO_WritePin(LED6_GPIO_Port, LED6_Pin, 0);
+  		  HAL_GPIO_WritePin(LED7_GPIO_Port, LED7_Pin, 1);
+  		  HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, 0);
+  		  HAL_GPIO_WritePin(LED12_GPIO_Port, LED12_Pin, 0);
+  		  HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, 1);
+  		  count5s++;
+  		  }
+  		  count++;
+  	  }
+  	  //led green tren va duoi on
+  	  else if(count <= 8){
+  		  HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, 0);
+  		  HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, 1);
+  		  HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, 0);
 
-    /* USER CODE END WHILE */
+  		  HAL_GPIO_WritePin(LED5_GPIO_Port, LED5_Pin, 1);
+  		  HAL_GPIO_WritePin(LED6_GPIO_Port, LED6_Pin, 0);
+  		  HAL_GPIO_WritePin(LED7_GPIO_Port, LED7_Pin, 0);
 
-    /* USER CODE BEGIN 3 */
-  }
+  		  HAL_GPIO_WritePin(LED8_GPIO_Port, LED8_Pin, 0);
+  		  HAL_GPIO_WritePin(LED9_GPIO_Port, LED9_Pin, 1);
+  		  HAL_GPIO_WritePin(LED10_GPIO_Port, LED10_Pin, 0);
+
+  		  HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, 1);
+  		  HAL_GPIO_WritePin(LED12_GPIO_Port, LED12_Pin, 0);
+  		  HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, 0);
+  		  count++;
+  	  }
+  	  //led yellow tren va duoi on
+  	  else if(count <= 9){
+  		  HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, 0);
+  		  HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, 0);
+  		  HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, 1);
+
+  		  HAL_GPIO_WritePin(LED5_GPIO_Port, LED5_Pin, 1);
+  		  HAL_GPIO_WritePin(LED6_GPIO_Port, LED6_Pin, 0);
+  		  HAL_GPIO_WritePin(LED7_GPIO_Port, LED7_Pin, 0);
+
+  		  HAL_GPIO_WritePin(LED8_GPIO_Port, LED8_Pin, 0);
+  		  HAL_GPIO_WritePin(LED9_GPIO_Port, LED9_Pin, 0);
+  		  HAL_GPIO_WritePin(LED10_GPIO_Port, LED10_Pin, 1);
+
+  		  HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, 1);
+  		  HAL_GPIO_WritePin(LED12_GPIO_Port, LED12_Pin, 0);
+  		  HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, 0);
+  		  count++;
+  	  }
+  	  else{
+  		  count = 1;
+  		  count5s = 1;
+  	  }
+  	  HAL_Delay(1000);
+      /* USER CODE END WHILE */
+
+      /* USER CODE BEGIN 3 */
+    }
   /* USER CODE END 3 */
 }
 
